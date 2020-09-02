54495ea501c3031be5ebf236aabb904174da353e
Taha Akhtar <taha-akhtar13@hotmail.com>
2020-09-02 08:31:44 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React, { useState,useEffect } from "react";
+import React, { useState,useEffect,useRef } from "react";
 import {
   FormControl,
   Input,
-  IconButton
+  IconButton,
+  Card,
+  CardActions,
+  FormLabel,
+  Paper,
+Grid,
+  TextField,
+  AppBar,
+  Toolbar,
+  Button,
+  Typography,
+  CardContent,
+  CardHeader,
+  
 } from "@material-ui/core";
 import Message from "./Message"
 import  "./App.css";
 import db from "./firebase";
 import firebase from "firebase"
 import FlipMove from "react-flip-move"
-import SendIcon from '@material-ui/icons/Send';
+import SendIcon  from '@material-ui/icons/Send';
+import MenuIcon from '@material-ui/icons/Menu';
 
 function App() {
   const [input, setInput] = useState("");
@@ -18,14 +32,20 @@ function App() {
 
   const [userName,setUserName]=useState([]);
 
+  const[islogin,setLogin]=useState(false);
 
+  const messagesEndRef = useRef(null);
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  };
 
-  useEffect(()=>{
-    setUserName(prompt("Enter your user"))
-  },[]);
+  // useEffect(()=>{
+  //   setUserName(prompt("Enter your user"))
+  // },[]);
 
 useEffect(()=>{
 
+  
    db.collection('message').orderBy('timestamp','asc').onSnapshot(snapshot=>{
     setMessage(snapshot.docs.map(
         doc=>({ id:doc.id, message: doc.data()})
@@ -34,6 +54,8 @@ useEffect(()=>{
 
 },[]);
 
+
+useEffect(scrollToBottom, [messages]);
 
 
   const sendMessage = (event) => {
@@ -46,19 +68,80 @@ useEffect(()=>{
 
     });
     setInput("");
-    // const Id=lastId+1;
-    // setLastId(Id);
-    // console.log(Id);
-    // setMessage([...messages, { id:Id,  username:userName, message: input }]);
-    // setInput("");
   };
 
   return (
+
+
+
+
+
     <div className="App">
+
+
+<div  className={'login ' + (islogin && 'card__hidden')} >
+<Grid container spacing={3}>
+        <Grid item xs={4}>
+          &nbsp;
+        </Grid>
+
+        <Grid item xs={3}>
+        <AppBar position="static">
+  <Toolbar>
+    
+    <Typography variant="h6" >
+      Login
+    </Typography>
+    
+  </Toolbar>
+</AppBar>
+<FormControl className="app__form">
+  <Grid container spacing={1} >
+  <Grid item xs={9}>
+  <Input 
+  placeholder="Enter name"
+          variant="contained"
+          type="text"
+          value={userName}
+          className="input__form"
+          onChange={(event)=>{ setUserName(event.target.value);}}
+        />
+  </Grid>
+  <Grid item xs={3}>
+  <IconButton color="primary" aria-label="Send Message" 
+          disabled={!userName}
+          className="app__iconButton"
+          onClick={event=>setLogin(true)} autoFocus  >
+  <SendIcon />
+  </IconButton>
+  </Grid>
+  </Grid>
+  </FormControl>
+        </Grid>
+        </Grid>
+</div>
+
+
+    <Card className={ !islogin && 'card__hidden'} > 
       <h2>The Chatter Box</h2>
       <h2>Welcome {userName}</h2>
 
-      <form onSubmit={sendMessage} className="app__form" >
+      
+        <FlipMove>
+        {
+
+         
+          messages.map(({id,message})  => (
+           
+          <Message name={userName} message={message} key={id} />
+
+          ))
+        }
+      </FlipMove>
+
+
+      <CardActions>
+           <form onSubmit={sendMessage} className="app__form" >
       <FormControl className="app__formControl" >
         <Input
         placeholder="Enter a message"
@@ -71,25 +154,17 @@ useEffect(()=>{
         <IconButton color="primary" aria-label="Send Message" 
           disabled={!input}
           className="app__iconButton"
-          onClick={sendMessage}  >
+          onClick={sendMessage} autoFocus  >
   <SendIcon />
 </IconButton>
 
         </FormControl>
-
+        <div ref={messagesEndRef} />
         </form>
+      </CardActions>
 
-        <FlipMove>
-        {
 
-         
-          messages.map(({id,message})  => (
-           
-          <Message name={userName} message={message} key={id} />
-
-          ))
-        }
-      </FlipMove>
+      </Card>
     </div>
   );
 }
